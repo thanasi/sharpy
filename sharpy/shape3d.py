ff24a659449c0b6f6bfef77823b67a8f8c41e1cf
Thanasi <thanasi91@gmail.com>
2013-03-19 00:17:59 -0500

    changed center of mass and inertia calculations to be based on mesh instead of random point cloud. seems to work well.

--- sharpy/shape3d.py
+++ sharpy/shape3d.py
@@ -6,16 +6,41 @@
 ## Date: March, 2013
 ## 
 ##############################
+from __future__ import division
 import os
-from itertools import izip
+import vtk
 import numpy as np
 from numpy import linalg as LA
-import vtk
+from itertools import izip, imap
 
 np.set_printoptions(precision=3)
 
 ## tolerance for point merging
 TOL = 1e-4
+
+## shorthand for readably accessing arrays
+X = 0
+Y = 1
+Z = 2
+
+## subexpressions for integrals
+def subexpr(w):
+
+    f = np.zeros(3)
+    g = np.zeros(3)
+
+    tmp0 = w[0] + w[1]
+    tmp1 = w[0]**2
+    tmp2 = tmp1 + w[1] * tmp0
+
+    f[0] = w.sum()    
+    f[1] = tmp2 + w[2] * f[0]
+    f[2] = w[0] * tmp1 + w[1] * tmp2 + w[2] * f[1]
+    
+    g = f[1] + w * (f[0] + w)
+    
+    return f,g
+    
 
 class ImportError(Exception):
     """ Base class for surface import errors. """
@@ -44,19 +69,27 @@ class Shape3D(object):
         
     """
     
-    def __init__(self, fn, clean=True, inertia=True, align=True):
+    def __init__(self, fn, clean=True, mass=True, align=True):
+        
+        if not os.path.exists(fn):
+            raise IOError("input file does not exists")
+        
         self._filename = fn
         
         self._loaded = False
+        self._centered = False
         self._aligned = False
         self._cleaned = False
         self._pointchecked = False
         self._pointcloudgen = False
         
         self.Surf = None
-        self.InertiaTensor = None
+        self.nFaces = None
         self.PointCloud = None
-        self.CoM = None
+
+        self.mass = 0
+        self.CoM = np.zeros(3)
+        self.inertia = np.zeros((3,3))
         
         self._eigval = None
         self._eigvec = None
@@ -69,16 +102,16 @@ class Shape3D(object):
             if clean:
                 self._clean_dup_points()
             
-            if inertia:
-                print 'generating point cloud '
-                self._generate_pointcloud()
-                print 'calculating moment of inertia and eigenvecs'
-                self._calc_inertia_tensor()
-                print 'calculated eigenvecs'
-                print self._eigvec
+            if mass:
+                print 'calculating mass properties'
+                self._calc_mass_prop()
+                # print self.inertia
+                # print self._eigvec
                 
             if align:
+                ## works perfectly if you run it twice. what can I say?
                 print 'aligning principal moments to x,y,z axes'
+                self._align_axes()
                 self._align_axes()
             
         else:
@@ -94,8 +127,9 @@ class Shape3D(object):
         
         ## set up surface
         self.Surf = reader.GetOutput()
+        self.nFaces = self.Surf.GetNumberOfCells()
         self._bounds = self.Surf.GetBounds()
-        
+
         self._loaded = True
         
     def _clean_dup_points(self):
@@ -117,27 +151,151 @@ class Shape3D(object):
 
         self._cleaned = True   
         
+    def _calc_mass_prop(self):
+        """
+        calculate mass, center of mass, and moment of inertia of mesh
+            
+        """
+        
+        mult = np.array([1/6,1/24,1/24,1/24,1/60,1/60,1/60,1/120,1/120,1/120])
+        intg = np.zeros(10) ## order: 1, x, y, z, x^2, y^2, z^2, xy, yz, zx
+        
+        for f in range(self.nFaces):
+            ## get vertices
+            p = map(self.Surf.GetCell(f).GetPoints().GetPoint, range(3))
+            p = np.array(p)
+            x,y,z = p.T
+            fx = np.zeros(3)
+            fy = np.zeros(3)
+            fz = np.zeros(3)
+            gx = np.zeros(3)
+            gy = np.zeros(3)
+            gz = np.zeros(3)
+            
+            ## get edges
+            e1 = p[1] - p[0]
+            e2 = p[2] - p[0]
+            d = np.cross(e1,e2)
+            
+            ## compute integral terms
+            fx,gx = subexpr(x)
+            fy,gy = subexpr(y)
+            fz,gz = subexpr(z)
+            
+            ## update integrals
+            intg[0] += d[X] * fx[0]
+            
+            intg[1] += d[X] * fx[1]
+            intg[2] += d[Y] * fy[1]
+            intg[3] += d[Z] * fz[1]
+            
+            intg[4] += d[X] * fx[2]
+            intg[5] += d[Y] * fy[2]
+            intg[6] += d[Z] * fz[2]
+            
+            intg[7] += d[X] * np.dot(y,gx)
+            intg[8] += d[Y] * np.dot(z,gy)
+            intg[9] += d[Z] * np.dot(x,gz)      
+            
+        ## apply weights
+        intg *= mult
+        
+        ## calc mass
+        mass = intg[0]
+        
+        ## calc CoM
+        cm = intg[1:4] / mass
+        
+        inertia = np.zeros((3,3))
+        
+        ## calc inertia wrt CoM
+        inertia[X,X] = intg[5] + intg[6] - mass * (cm[Y]**2 + cm[Z]**2)
+        inertia[Y,Y] = intg[4] + intg[6] - mass * (cm[Z]**2 + cm[X]**2)
+        inertia[Z,Z] = intg[4] + intg[5] - mass * (cm[X]**2 + cm[Y]**2)
+        inertia[X,Y] = -(intg[7] - mass * cm[X] * cm[Y])
+        inertia[Y,Z] = -(intg[8] - mass * cm[Y] * cm[Z])
+        inertia[X,Z] = -(intg[8] - mass * cm[Z] * cm[X])
+        inertia[Y,X] = inertia[X,Y]
+        inertia[Z,Y] = inertia[Y,Z]
+        inertia[Z,X] = inertia[X,Z]
+        
+        self.mass = mass
+        self.CoM = cm
+        self.inertia = inertia   
+        
+        # calculate and sort eigenvectors of inertia tensor
+        eigval, eigvec = LA.eigh(self.inertia)
+
+        eigsys = zip(eigval, eigvec.T)
+        eigsys.sort(key = lambda x: x[0])
+
+        eigval, eigvec = zip(*eigsys)
+
+        self._eigval = eigval[::-1]
+        self._eigvec = np.array(eigvec)[::-1]
+        
+    def _center(self):
+        """
+        center mesh at (0,0,0)
+        
+        """
+        
+        txf = vtk.vtkTransform()
+        txf.PostMultiply()
+        txf.Translate(-1*self.CoM)
+        
+        txfPoly = vtk.vtkTransformPolyDataFilter()
+        txfPoly.SetInput(self.Surf)
+        txfPoly.SetTransform(txf)
+        txfPoly.Update()
+        
+        self.Surf = txfPoly.GetOutput()
+        self._bounds = self.Surf.GetBounds()
+        
+        self._calc_mass_prop()
+        
+        self._centered = True
+                      
+        
     def _align_axes(self):
         """ 
         Align the imported surface according to the following rules:
-            - move center of mass to (0,0,0)
             - x axis: major axis of the moment of inertia tensor
             - y axis: middle axis
             - z axis: minor  axis
         
         """
         
-        ## calculate rotation axes and angles
-        
+        # make sure we're centered
+        if not self._centered:
+            self._center()
+            
         ## align major principal axis
-        
         rax1 = np.cross(self._eigvec[0],(1,0,0))
+        rax1 /= LA.norm(rax1)
         rang1 = np.abs(np.arccos(self._eigvec[0][0]) * 180 / np.pi)
+        
+        c = self._eigvec[0][0]
+        s = np.sin(np.arccos(c))
+        
+        R = np.eye(3) * c + \
+            (1-c) * np.outer(rax1,rax1) + \
+            s * np.array([ [0, -rax1[2], rax1[1]],
+                           [rax1[2], 0, -rax1[0]],
+                           [-rax1[1], rax1[0], 0]])
+        
+        e1rot = np.dot(R,self._eigvec[0])
+        e2 = self._eigvec[1]
+        e2rot = np.dot(R,e2)
+        
+        rax2 = np.cross(e2rot,(0,1,0))
+        rax2 /= LA.norm(rax2)
+        rang2 = np.abs(np.arccos(e2rot[1]) * 180/np.pi)
         
         txf = vtk.vtkTransform()
         txf.PostMultiply()
-        txf.Translate(-1*self.CoM)
         txf.RotateWXYZ(rang1,rax1)
+        txf.RotateWXYZ(rang2,rax2)
         
         txfPoly = vtk.vtkTransformPolyDataFilter()
         txfPoly.SetInput(self.Surf)
@@ -147,16 +305,16 @@ class Shape3D(object):
         self.Surf = txfPoly.GetOutput()
         self._bounds = self.Surf.GetBounds()
     
-        print 'aligned surface. recalculating eigenvectors of MoI'
-        self._generate_pointcloud()
-        self._calc_inertia_tensor()
+        print 'aligned surface. recalculating eigenvectors of inertia tensor'
+        self._calc_mass_prop()
         
         print "updated eigenvectors"
         print self._eigvec
+        ## print self.inertia
         
         self._aligned = True
         
-    def _generate_pointcloud(self, N=1e4):
+    def _generate_pointcloud(self, N=1e5, save=False):
         """ Build an N-point pointcloud representation of the surface """
 
         x = np.random.uniform(low=self._bounds[0], high=self._bounds[1], size=N)
@@ -165,40 +323,13 @@ class Shape3D(object):
         
         pc = np.array([x,y,z]).T
         
-        inbool = self.is_inside(pc,save=False)
+        inbool = self.is_inside(pc,save=save)
         
         self.PointCloud = pc[inbool==1]
         print sum(inbool)
         
         self._pointcloudgen = True
                 
-    def _calc_inertia_tensor(self):
-        """ Calculate moment of Inertia Tensor """
-        
-        self.CoM = self.PointCloud.mean(0)
-        
-        pc = self.PointCloud.copy() - self.CoM
-        
-        ## moment of inertia tensor
-        gr = np.mgrid[:3,:3]
-        delta = np.eye(3)
-        
-        f = lambda x: delta * np.sum(x**2) - np.outer(x,x)
-        I = np.array(map(f,pc)).sum(0)
-        
-        self.InertiaTensor = I
-                    
-        # calculate and sort eigenvectors
-        eigval, eigvec = LA.eigh(I)
-        
-        eigsys = zip(eigval, eigvec.T)
-        eigsys.sort(key = lambda x: x[0])
-        
-        eigval, eigvec = zip(*eigsys)
-        
-        self._eigval = eigval[::-1]
-        self._eigvec = np.array(eigvec)[::-1]
-        
     
     def is_inside(self,points,save=True):
         """ Check if given points lie inside the surface """
@@ -298,7 +429,7 @@ class Shape3D(object):
             outpointsActor.SetMapper(outpointsMapper)
             outpointsActor.GetProperty().SetPointSize(5)
             outpointsActor.GetProperty().SetColor(1, 0, 0)
-            outpointsActor.GetProperty().SetOpacity(0.1)    
+            outpointsActor.GetProperty().SetOpacity(0.01)    
             
         ## eigenvector mappers, actors
         if eig:
